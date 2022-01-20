a2e0e5cb8243b2dadb818b1a522a9dde4679b096
Peter Melian <pmelian@silica.net>
2022-01-20 09:26:29 +0200

    Create Class DerbyAccess. Used to handle all Derby connections (Open connection nd close connection) Default CRUD layer for DB interaction.

---
+++ ChessClubWindowsApp/src/data/DerbyAccess.java
@@ -0,0 +1,130 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class DerbyAccess {
+	
+	/** Derby JDBC Connection URL **/
+	private static final String JDBC_URL = "jdbc:derby:ChessClub;create=true";
+	
+	private static Statement staListUser 	= null;
+	private static ResultSet resListUser 	= null;
+	
+	/** Getter and Setter **/
+	public static Statement getStaListUser() 				 { return staListUser;	}
+	public static void setStaListUser(Statement staListUser) { DerbyAccess.staListUser = staListUser;	}
+	public static ResultSet getResListUser() 				 {	return resListUser;	}
+	public static void setResListUser(ResultSet resListUser) {	DerbyAccess.resListUser = resListUser;	}
+	
+	public static void cleanUpStaticVar() {
+		if(getStaListUser() != null) {
+			try {
+				getStaListUser().close();
+				setStaListUser(null);
+			} catch (Throwable ignore) {}
+		}
+		if(getResListUser() != null) {
+			try {
+				getResListUser().close();
+				setResListUser(null);
+			} catch (Throwable ignore) {}
+		}
+	}
+	
+	/**
+	 * @author pmelian
+	 * @param conn
+	 * @throws Exception
+	 * @Comment 
+	 *          Creates table 'CHESSPLAYERS' if it does not exists
+	 *          with generated 'id' as UNIQUE identity
+	 *          Columns::
+	 *                   id 			( int )
+	 *                   FullName 		( varchar (256) )
+	 *                   EmailAddress   ( varchar (125) )
+	 *                   BirthDay       ( varchar (126) )
+	 *                   GamesPlayed	( int )
+	 *                   Rank			( int )
+	 *          
+	 */
+	public static void createDbTable(Connection conn) throws Exception {
+		DatabaseMetaData metaData	= null;
+		ResultSet resMetaData		= null;
+		boolean bTableExists		= false;
+		Statement stCreateTable    	= null;	
+		String cTableSql ="Create Table CHESSPLAYERS ("
+				+ "id int not null generated always as identity,"
+				+ "FullName varchar(256),"
+				+ "EmailAddress varchar(125),"
+				+ "BirthDay varchar (126),"
+				+ "GamesPlayed int,"
+				+ "Rank int"
+				+ ")";
+		try {
+			metaData = conn.getMetaData();
+			resMetaData = metaData.getTables(null, null, "CHESSPLAYERS",   new String[] {"TABLE"});
+			while (resMetaData.next()) {	 bTableExists =true;	 }
+			if(!bTableExists) {
+				stCreateTable = conn.createStatement();
+				stCreateTable.executeLargeUpdate(cTableSql);
+			}
+		} catch (SQLException e) {
+			throw new Exception("Derby create table exception," + e.getMessage());
+		}finally {
+			if(stCreateTable != null) {
+				try {
+					stCreateTable.close();
+					stCreateTable = null;
+				} catch (Throwable ignore) {}
+				
+				if(resMetaData != null) {
+					try {
+						resMetaData.close();
+						resMetaData = null;
+					} catch (Throwable ignore) {}
+				}
+			}
+		}
+		
+	}
+	
+	
+	/**
+	 * @author pmelian
+	 * @return Connection Object
+	 * @throws Exception
+	 * @Comment Open Connection to embedded Derby Data Base.
+	 * 
+	 */
+	public static Connection connectToDb() throws Exception {
+		Connection conn				 = null;
+		try {
+			conn = DriverManager.getConnection(JDBC_URL);
+			} catch (SQLException e) {
+			throw new Exception("Failed to connect to embedded Derby Data Base");
+		}
+		return conn;
+	}
+	
+	/**
+	 * @author pmelian
+	 * @param conn 
+	 * @Comment Close Derby Connection
+	 */
+	public static void closeDb(Connection conn) {
+		/** Clean up static variables that may be in use before closing the DB connection */
+		cleanUpStaticVar();
+		if (conn != null) {
+			try {
+				conn.close();
+				conn = null;
+			} catch (Throwable ignore) {}
+		}
+	}
+
+}
--- ChessClubWindowsApp/src/gui/InitFrame.java
+++ ChessClubWindowsApp/src/gui/InitFrame.java
@@ -1,12 +1,21 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import data.DerbyAccess;
+
+import java.sql.Connection;
 
 public class InitFrame {
 
 	private JFrame frame;
+	private JLabel dbLabelMsg 	= null;
+	private Connection conn 	= null;
 
 	/**
 	 * Launch the application.
@@ -29,15 +38,36 @@ public class InitFrame {
 	 */
 	public InitFrame() {
 		initialize();
+		
+		try {
+			conn = DerbyAccess.connectToDb();
+			DerbyAccess.createDbTable(conn);
+			dbLabelMsg.setText("Connected to embedded Derby Data Base. (Table CHESSPLAYERS)");
+		} catch (Exception e) {
+			dbLabelMsg.setText(e.getMessage());
+		}
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				System.out.println("Closing ..................................");
+				DerbyAccess.closeDb(conn);
+			}
+		});
+		frame.setBounds(100, 100, 959, 569);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		dbLabelMsg = new JLabel("..Initilizing DB");
+		dbLabelMsg.setBounds(153, 15, 420, 14);
+		frame.getContentPane().add(dbLabelMsg);
 	}
 
 }
